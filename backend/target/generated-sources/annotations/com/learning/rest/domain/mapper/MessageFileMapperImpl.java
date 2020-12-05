908981ec5f9907c0e180ed5a2bfc66ca212a5e12
Jakub <gudzikoff@gmail.com>
2020-12-05 11:35:25 +0100

    Update jar

--- backend/target/generated-sources/annotations/com/learning/rest/domain/mapper/MessageFileMapperImpl.java
+++ backend/target/generated-sources/annotations/com/learning/rest/domain/mapper/MessageFileMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-12-02T19:33:20+0100",
+    date = "2020-12-05T11:30:39+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
